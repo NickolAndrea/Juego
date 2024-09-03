7ee3302707ac2bcdebd1edbb889d0f835ca5a1ee
NickolAndrea <nickolandreaescobar@iemmmrip.edu.co>
2024-09-03 12:34:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var ne=math.floor(math.random() * (50 - 1) + 1);
+var ne=Math.floor(Math.random() * (50 - 1) + 1);
 function adivinar(){
     var nd=document.getElementById("barra").value;
     if(ne==nd){
-        alert("ADIVINASTE");
+        alert("GANASTE");
     }else if(ne>nd){
-        alert("el número es mayor");
+        alert("EL NÙMERO ES MAYOR");
     }else {
-        alert("el número es menor");
+        alert("EL MÙMERO ES MENOR");
     }
 }
